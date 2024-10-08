a5af479854b618db8f540f8dbbafacc57bf46c05
Fedor Dolmatov <l1sZ@ya.ru>
2024-10-08 23:25:19 +0300

    calculator fix

--- backend/src/calculator.py
+++ backend/src/calculator.py
@@ -6,33 +6,33 @@ class Calculator:
     def __init__(self):
         pass
 
-    def addition(self, x1: int | float, x2: int | float):
+    def addition(self, x1, x2):
         return x1 + x2
 
-    def multiplication(self, x1: int | float, x2: int | float):
+    def multiplication(self, x1, x2):
         return x1 * x2
 
-    def subtraction(self, x1: int | float, x2: int | float):
+    def subtraction(self, x1, x2):
         return x1 - x2
 
-    def division(self, x1: int | float, x2: int | float):
+    def division(self, x1, x2):
         if x2 != 0:
             return x1 / x2
 
-    def absolute(self, x: int | float):
+    def absolute(self, x):
         return abs(x)
 
-    def degree(self, x: int | float, n: int | float):
+    def degree(self, x, n):
         return x ** n
 
-    def ln(self, x: int | float):
+    def ln(self, x):
         return math.log(x)
 
-    def log(self, x: int | float, n: int | float):
+    def log(self, x, n):
         return math.log(x, n)
 
-    def sqrt(self, x: int | float):
+    def sqrt(self, x):
         return x ** 0.5
 
-    def nth_root(self, x: int | float, n: int | float):
+    def nth_root(self, x, n):
         return x ** (1. / n)
--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -11,14 +11,20 @@ class TestCalculator(unittest.TestCase):
     def test_addition_integers(self):
         self.assertEqual(self.calculator.addition(1, 2), 3)
 
-    def test_addition_integers_polar(self):
+    def test_addition_integers_different_signs(self):
         self.assertEqual(self.calculator.addition(1, -2), -1)
 
     def test_addition_floats(self):
         self.assertAlmostEqual(self.calculator.addition(1.5, 2.5), 4.0)
-
-    def test_addition_floats_polar(self):
+    
+    def test_addition_floats_different_signs(self):
         self.assertAlmostEqual(self.calculator.addition(-1.5, -2.5), -4.0)
+    
+    def test_addition_complex(self):
+        self.assertAlmostEqual(self.calculator.addition((1.5 + 2j), (-2 + 2.2j)), -0.5 + 4.2j)
+
+    def test_addition_wrong_type(self):
+        self.assertRaises(TypeError, self.calculator.addition, 'a', 1)
 
     def test_addition_zero(self):
         self.assertEqual(self.calculator.addition(0, 0), 0)
@@ -30,11 +36,20 @@ class TestCalculator(unittest.TestCase):
     def test_subtraction_integers(self):
         self.assertEqual(self.calculator.subtraction(5, 2), 3)
     
-    def test_subtraction_integers_polar(self):
-        self.assertEqual(self.calculator.subtraction(-1, -2), 1)
+    def test_subtraction_integers_different_signs(self):
+        self.assertEqual(self.calculator.subtraction(1, -2), 3)
 
     def test_subtraction_floats(self):
-        self.assertEqual(self.calculator.subtraction(5.5, 2.5), 3.0)
+        self.assertAlmostEqual(self.calculator.subtraction(1.5, 2.5), -1.0)
+    
+    def test_subtraction_floats_different_signs(self):
+        self.assertAlmostEqual(self.calculator.subtraction(1.5, -2.5), 4.0)
+    
+    def test_subtraction_complex(self):
+        self.assertAlmostEqual(self.calculator.subtraction((1.5 + 2j), (-2 + 2.2j)), 3.5 - 0.2j)
+
+    def test_subtraction_wrong_type(self):
+        self.assertRaises(TypeError, self.calculator.subtraction, 'a', 1)
 
     def test_subtraction_zero(self):
         self.assertEqual(self.calculator.subtraction(0, 0), 0)
@@ -49,6 +64,12 @@ class TestCalculator(unittest.TestCase):
     def test_multiplication_floats(self):
         self.assertEqual(self.calculator.multiplication(2.5, 4.0), 10.0)
 
+    def test_multiplication_complex(self):
+        self.assertAlmostEqual(self.calculator.multiplication((1.5 + 2j), (-2 + 2.2j)), -7.4 - 0.7j)
+
+    def test_multiplication_wrong_type(self):
+        self.assertEqual(self.calculator.multiplication('a', 1), 'a') # из-за такого теста были введены аннотации :/
+
     def test_multiplication_zero(self):
         self.assertEqual(self.calculator.multiplication(5, 0), 0)
 
@@ -61,6 +82,12 @@ class TestCalculator(unittest.TestCase):
 
     def test_division_floats(self):
         self.assertEqual(self.calculator.division(6.0, 2.0), 3.0)
+
+    def test_division_complex(self):
+        self.assertAlmostEqual(self.calculator.division((1.5 + 2j), (-2 + 2.2j)), (0.15837104072398192-0.8257918552036199j))
+
+    def test_division_wrong_type(self):
+        self.assertRaises(TypeError, self.calculator.division, 1, 'a')
 
     def test_division_zero_divisor(self):
         self.assertIsNone(self.calculator.division(5, 0))
@@ -75,6 +102,12 @@ class TestCalculator(unittest.TestCase):
     def test_absolute_floats(self):
         self.assertEqual(self.calculator.absolute(-5.5), 5.5)
 
+    def test_absolute_complex(self):
+        self.assertAlmostEqual(self.calculator.absolute(1 - 1.35j), 1.68002976)
+    
+    def test_absolute_wrong_type(self):
+        self.assertRaises(TypeError, self.calculator.absolute, 'hehe')
+
     def test_absolute_zero(self):
         self.assertEqual(self.calculator.absolute(0), 0)
 
@@ -84,6 +117,12 @@ class TestCalculator(unittest.TestCase):
 
     def test_degree_floats(self):
         self.assertEqual(self.calculator.degree(2.5, 2), 6.25)
+    
+    def test_degree_complex(self):
+        self.assertAlmostEqual(self.calculator.degree(1 - 1.35j, 2), -0.8225 - 2.7j)
+    
+    def test_degree_wrong_type(self):
+        self.assertRaises(TypeError, self.calculator.degree, 1, 'hehe')
 
     def test_degree_zero(self):
         self.assertEqual(self.calculator.degree(5, 0), 1)
@@ -95,6 +134,12 @@ class TestCalculator(unittest.TestCase):
     def test_ln_positive(self):
         self.assertAlmostEqual(self.calculator.ln(1), 0.0)
 
+    def test_ln_complex(self):
+        self.assertRaises(TypeError, self.calculator.ln, 1j)
+
+    def test_ln_wrong_type(self):
+        self.assertRaises(TypeError, self.calculator.ln, 'a')
+
     def test_ln_infinity(self):
         self.assertEqual(self.calculator.ln(float('inf')), float('inf'))
 
@@ -105,6 +150,12 @@ class TestCalculator(unittest.TestCase):
     def test_log_base10(self):
         self.assertAlmostEqual(self.calculator.log(100, 10), 2)
 
+    def test_log_complex(self):
+        self.assertRaises(TypeError, self.calculator.log, 1j, 2)
+
+    def test_log_wrong_type(self):
+        self.assertRaises(TypeError, self.calculator.log, 2, 'a')
+
     def test_log_base_invalid(self):
         self.assertRaises(ValueError, self.calculator.log, 100, -10)
 
@@ -114,6 +165,12 @@ class TestCalculator(unittest.TestCase):
 
     def test_sqrt_zero(self):
         self.assertEqual(self.calculator.sqrt(0), 0)
+    
+    def test_sqrt_complex(self):
+        self.assertAlmostEqual(self.calculator.sqrt(1j), 0.707106781 + 0.707106781j)
+
+    def test_sqrt_wrong_type(self):
+        self.assertRaises(TypeError, self.calculator.nth_root, 'a')
 
     def test_sqrt_negative(self):
          self.assertAlmostEqual(self.calculator.sqrt(-9), (1.8e-16 + 3j))
@@ -121,6 +178,12 @@ class TestCalculator(unittest.TestCase):
     # Тесты для корня n-й степени
     def test_nth_root(self):
         self.assertEqual(self.calculator.nth_root(27, 3), 3)
+
+    def test_nth_root_complex(self):
+        self.assertAlmostEqual(self.calculator.nth_root(1j, 2), 0.707106781 + 0.707106781j)
+
+    def test_nth_root_wrong_type(self):
+        self.assertRaises(TypeError, self.calculator.nth_root, 'a', 2)
 
     def test_nth_root_negative(self):
         self.assertAlmostEqual(self.calculator.nth_root(-27, 3), (1.5 + 2.598076211353316j))
